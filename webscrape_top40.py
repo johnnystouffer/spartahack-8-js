90bb84cc2d978f0e9ea8f16aefc58f0ba97b24c0
jack <jackbutzu@gmail.com>
2023-01-28 17:22:34 -0500

    Changes data formatting to emit numbers. Now we can try to seperate again

--- webscrape_top40.py
+++ webscrape_top40.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
-
+from string import digits
 res = requests.get('https://top40weekly.com/all-us-top-40-singles-for-2023/#January')
 #print(res.text)
 soup = BeautifulSoup(res.content,'html.parser')
@@ -18,6 +18,16 @@ print(text.index("THIS WEEK’S DROP – JANUARY 7, 2023"))
 
 text = text[1782:4676]
 
-print(type(text))
-text = text.split("–•–")
-print(text)
+
+# #print(type(text))
+# #text = text.split("(")
+# text = str(text)
+# text = text.split('–•–')
+# print(text)
+
+newtext = text.translate({ord(k): None for k in digits})
+#print(type(text))
+newtext = newtext.split("()")
+newtext = str(newtext)
+newtext = newtext.split('–•–')
+print(newtext)
